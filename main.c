48b1d65caa4aa9b79315dda9c0b0231d7a259587
Elijah Quinn <118429416+eliquinn@users.noreply.github.com>
2026-03-25 01:28:15 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void input(char *str);
+char opt[64];
+int loc =0;
+
+void loop()
+{
+  switch(loc) {
+case 1:
+  }}
+
+void input(char *str)
+{
+printf(">");
+  fgets(str, 64, stdin);
+  str[strcspn[str, "\n")] = '\0';
+}
+
+int main()
+{
+  input(opt);
+printf("You said %s", fart);
+return 0;
+}
