24eba92cc2e00769c68d66067438a1cd65f6a016
eliquinn <118429416+eliquinn@users.noreply.github.com>
2026-03-25 03:03:34 -0700

    more

--- main.c
+++ main.c
@@ -4,25 +4,40 @@
 #include <string.h>
 
 void input(char *str);
+int cmd(char *str);
 char opt[64];
-int loc =0;
+int loc = 1;
 
-void loop()
-{
-  switch(loc) {
-case 1:
-  }}
-
-void input(char *str)
-{
-printf(">");
-  fgets(str, 64, stdin);
-  str[strcspn[str, "\n")] = '\0';
+void loop() {
+    while(1) {
+        if (cmd("loud")) {
+            printf("AAAAAA\n");
+        } else if (cmd("soft")) {
+            printf("aaaa...\n");
+        } else {
+            printf("no command\n");
+        }
+        input(opt);
+    }
 }
 
-int main()
-{
-  input(opt);
-printf("You said %s", fart);
-return 0;
+int cmd(char *str) {
+    if (strcmp(opt, str) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void input(char *str) {
+    printf(">");
+    fgets(str, 64, stdin);
+    str[strcspn(str, "\n")] = '\0';
+}
+
+int main() {
+    input(opt);
+    printf("You said %s", opt);
+    loop();
+    return 0;
 }
