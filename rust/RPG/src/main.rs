bae9ce86550e33ca2c53f9859f264fd896f80d60
eliquinn <118429416+eliquinn@users.noreply.github.com>
2026-03-30 03:02:42 -0700

    a

--- rust/RPG/src/main.rs
+++ rust/RPG/src/main.rs
@@ -1,13 +1,48 @@
-use std::io;
+use std::io::{self, Write};
+
+
+fn read_input() -> String {
+    print!(">");
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    
+    /*return*/ input.trim().to_string()
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let mut location = "House"; 
 
-    let mut input = String::new();
+    loop {
+        match location.as_ref() {
+            "House" => {
+                println!("You are at home, what would you like to do?");
+                println!("open door, go upstairs");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+                //Matching The Input
+                match read_input().as_str() {
+                    "open door" => println!("You open the door"),
+                    "go upstairs" => { 
+                        println!("You go upstairs"); 
+                        location = "Upstairs"; 
+                    }
+                    _ => println!("That is not a command"),
+                }
+            }
+            "Upstairs" => {
+                println!("There is nothing upstairs.");
+                println!("go downstairs");
 
-    println!("You entered: {}", input.trim());
+                match read_input.as_str() {
+                    "go downstairs" => { 
+                        println!("You go downstairs"); 
+                        location = "House"; 
+                    }
+                    _ => println!("That is not a command"),
+                }
+            }
+            _ => println!("idk"),
+        }
+    }
 }
